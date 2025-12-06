05ca87136d966eef2478e4f1b365f73dd5875fea
Gulshan Kumari <gulshan.kumari2207@gmail.com>
2025-12-07 00:08:27 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,7 @@ const Usage = require("./models/Usage");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server running on port ${PORT}`);
-// });
+
 
 // ✅ MIDDLEWARES
 app.use(
